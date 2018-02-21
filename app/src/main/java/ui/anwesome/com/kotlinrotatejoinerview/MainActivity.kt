310fec456933e2c5cf6dc74d90eb8d83238cfb9f
Anwesh43 <anweshthecool0@gmail.com>
2018-02-21 09:20:55 +0530

    fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinrotatejoinerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrotatejoinerview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinrotatejoinerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.rotatejoinerview.RotateJoinerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RotateJoinerView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
