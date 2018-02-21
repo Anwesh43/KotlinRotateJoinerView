58a183954c26bc245916a03d1b3ca2f22c8d72f1
Anwesh43 <anweshthecool0@gmail.com>
2018-02-21 08:57:06 +0530

    using Renderer in View

--- rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
+++ rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
@@ -9,13 +9,14 @@ import android.graphics.*
 import android.view.*
 class RotateJoinerView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = Renderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas, paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -127,7 +128,7 @@ class RotateJoinerView(ctx:Context):View(ctx) {
         fun create(activity: Activity): RotateJoinerView {
             val view = RotateJoinerView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
