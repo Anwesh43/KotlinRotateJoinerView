c70c9656e0f96da220ef5b057028fbcf512cd4e9
Anwesh43 <anweshthecool0@gmail.com>
2018-02-21 08:59:33 +0530

    adding RotateJoinerView in activity

--- app/src/main/java/ui/anwesome/com/kotlinrotatejoinerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrotatejoinerview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinrotatejoinerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.rotatejoinerview.RotateJoinerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotateJoinerView.create(this)
     }
 }
