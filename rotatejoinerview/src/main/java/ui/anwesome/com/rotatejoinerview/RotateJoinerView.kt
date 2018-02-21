9d8a4cd55c807acf316207b20928048162f5da9b
Anwesh43 <anweshthecool0@gmail.com>
2018-02-21 08:29:03 +0530

    adding RotateJoinerView class

--- rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
+++ rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.rotatejoinerview
 /**
  * Created by anweshmishra on 21/02/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+class RotateJoinerView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
