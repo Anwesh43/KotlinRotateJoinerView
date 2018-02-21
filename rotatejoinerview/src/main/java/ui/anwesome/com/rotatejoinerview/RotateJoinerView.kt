c251c89a6863258802c0a1cca0a61e9e55eaef16
Anwesh43 <anweshthecool0@gmail.com>
2018-02-21 08:31:29 +0530

    adding Animator class

--- rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
+++ rotatejoinerview/src/main/java/ui/anwesome/com/rotatejoinerview/RotateJoinerView.kt
@@ -19,4 +19,29 @@ class RotateJoinerView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class Animator(var view:View, var animated:Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
